9acc30747a079d7ed3012682eb4d4bcd3a752549
Teddy Kuo <tkuo93@gmail.com>
2026-07-10 19:07:01 -0400

    Fix SwimmerProfile model export - add missing module.exports

--- SwimCoach-project/src/models/SwimmerProfile.js
+++ SwimCoach-project/src/models/SwimmerProfile.js
@@ -276,3 +276,5 @@ swimmerProfileSchema.index({ experienceLevel: 1 });
 // Create and export the model
 const SwimmerProfile = mongoose.model('SwimmerProfile', swimmerProfileSchema);
 
+module.exports = SwimmerProfile;
+
