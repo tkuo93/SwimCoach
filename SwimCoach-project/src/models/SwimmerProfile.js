ff7be4dab7a0297e930a81dce472f38f6391491c
Teddy Kuo <tkuo93@gmail.com>
2026-07-10 17:23:47 -0400

    Force rebuild

--- SwimCoach-project/src/models/SwimmerProfile.js
+++ SwimCoach-project/src/models/SwimmerProfile.js
@@ -276,4 +276,4 @@ swimmerProfileSchema.index({ experienceLevel: 1 });
 // Create and export the model
 const SwimmerProfile = mongoose.model('SwimmerProfile', swimmerProfileSchema);
 
-module.exports = SwimmerProfile;
+module.exports = SwimmerProfile;// force rebuild
